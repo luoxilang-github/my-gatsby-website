9a68da59c8bea299d2074eddf1ccf1311119016c
xilang <1139869214@qq.com>
2022-07-29 15:37:33 +0800

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/Layout'
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
@@ -9,6 +10,14 @@ interface BlogPostProps {
       frontmatter: {
         date: string;
         title: string;
+        hero_image_alt: string;
+        hero_image_credit_link: string;
+        hero_image_credit_text: string;
+        hero_image: {
+          childImageSharp: {
+            gatsbyImageData: string;
+          }
+        };
       },
       body: string;
     }
@@ -16,9 +25,11 @@ interface BlogPostProps {
 }
 
 const BlogPost: React.FC<BlogPostProps> = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image as any)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      {image && <GatsbyImage alt={data.mdx.frontmatter.hero_image_alt} image={image} />}
       <MDXRenderer>
         {data.mdx.body}
       </MDXRenderer>
@@ -32,6 +43,14 @@ query($id: String) {
     frontmatter {
       title
       date(formatString: "MMMM D, YYYY")
+      hero_image_alt
+      hero_image_credit_link
+      hero_image_credit_text
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
     }
     body
   }
